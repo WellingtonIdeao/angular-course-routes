b1bfa88a4570fdf942f721fb47dd902a00c0100b
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-26 18:20:25 -0300

    Rotas filhas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { CursosModule } from './cursos/cursos.module';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    CursosModule
+    CursosModule,
+    AlunosModule
   ],  
   providers: [],
   bootstrap: [AppComponent]
